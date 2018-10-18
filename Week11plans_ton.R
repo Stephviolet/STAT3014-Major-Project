3be27781016b2303a0302a9a03f87a1d02631523
Ton Lo <gargoyle1919@users.noreply.github.com>
2018-10-18 18:36:04 +1100

    test for block independence

--- Week11plans_ton.R
+++ Week11plans_ton.R
@@ -1,12 +1,9 @@
 #Week 11
-setwd("C:/Users/luodo/OneDrive/Courses/STAT3914/Project")
+setwd("~/GitHub/STAT3014-Major-Project")
 #library(ggplot2)
 Data <- read.csv("cleanedData.csv")
 dim(Data)
 adultData<-Data[Data$AGEC>=18,]
-# low.fat_high.carb<-Data$FATPER1<25 & Data$CHOPER1>55
-# high.protein_low.carb<-Data$PROPER1>20 & Data$CHOPER1<45
-# high.fat_low.carb<-Data$FATPER1>30 & Data$CHOPER1<45
 carb.cat<-cut(adultData$CHOPER1,
               breaks=c(-1,45,65,100),
               labels=c('low','medium','high'))
@@ -16,8 +13,6 @@ fat.cat<-cut(adultData$FATPER1,
 protein.cat<-cut(adultData$PROPER1,
                  breaks=c(-1,15,25,100),
                  labels=c('low','medium','high'))
-mean(high.carb);mean(high.protein);mean(high.fat)
-adultData1<-cbind(adultData,high.carb,high.protein,high.fat)
 
 
 # Test independence of high.carb, high.protein, high.fat (log-linear models!)
@@ -28,3 +23,32 @@ adultData1<-cbind(adultData,high.carb,high.protein,high.fat)
 
 # "women might be more likely to adopt a low-fat diet?"
 # Logistic Regression! or LDA, CART
+
+# log-linear model
+tables.3way<-table(carb.cat,fat.cat,protein.cat)
+table.y<-NULL
+for(i in 1:3){
+  for(j in 1:3){
+    table.y<-c(table.y,tables.3way[j,,i])
+  }
+}
+table.fat<-factor(rep(c('low','medium','high'),9))
+table.carb<-factor(rep(c('low','medium','high'),each=3,times=3))
+table.protein<-factor(rep(c('low','medium','high'),each=9))
+loglin.dat<-data.frame(y=table.y,
+                          fat=table.fat,
+                          carb=table.carb,
+                          protein=table.protein)
+loglin.dat
+d.additive<-glm(y~carb+fat+protein,
+                family='poisson',data=loglin.dat)$dev
+d.additive
+glm.cf<-glm(y~carb+fat+protein+carb:fat,
+                family='poisson',data=loglin.dat)
+glm.cp<-glm(y~carb+fat+protein+carb:protein,
+          family='poisson',data=loglin.dat)
+glm.fp<-glm(y~carb+fat+protein+fat:protein,
+          family='poisson',data=loglin.dat)
+c(glm.cf$dev,glm.cp$dev,glm.fp$dev)
+anova(glm.fp,test='Chisq')
+glm(y~fat+carb+protein,family='poisson',data=loglin.dat)
